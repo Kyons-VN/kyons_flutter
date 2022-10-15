ee54a5952b0f28d6384ec81ca5b8e47b8517ba0b
mibi <binhhm2009@gmail.com>
2022-10-15 11:18:39 +0700

    Change main file to work with Appcircle

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:kyons_flutter/src/core/view/themes.dart';
+import 'package:kyons_flutter/boostrap/config_reader.dart';
+import 'package:kyons_flutter/boostrap/main_common.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // theme: ThemeData(
-      //   visualDensity: VisualDensity.standard,
-      //   textTheme: GoogleFonts.montserratTextTheme(
-      //     Typography().black.copyWith(
-      //           headlineMedium: const TextStyle(
-      //             fontWeight: FontWeight.w700,
-      //             color: AppColors.primaryBlue,
-      //           ),
-      //           bodyMedium: const TextStyle(
-      //             fontSize: AppFontSizes.paragraph,
-      //             fontWeight: FontWeight.w400,
-      //             color: AppColors.primaryBlue,
-      //           ),
-      //           titleMedium: const TextStyle(
-      //             color: AppColors.primaryBlue,
-      //           ),
-      //           labelMedium: const TextStyle(
-      //             color: AppColors.primaryBlue,
-      //           ),
-      //         ),
-      //   ),
-      // ),
-      theme: lightTheme(),
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(onPressed: () {}, child: const Text('Button')).large(context),
-              const SizedBox(height: 8),
-              ElevatedButton(onPressed: () {}, child: const Text('Button')).medium(context),
-              const SizedBox(height: 8),
-              ElevatedButton(onPressed: () {}, child: const Text('Button')).small(context),
-              const SizedBox(height: 8),
-              ElevatedButton(onPressed: () {}, child: const Text('Button')),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+Future<void> main() async {
+  await mainCommon(Environment.prod);
 }
